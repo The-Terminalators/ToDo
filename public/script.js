44a2bc80c3f3f1f98b3025e7b268b87421eb4eb3
Lilly Ghassemieh <lillygh@gmail.com>
2015-11-17 18:24:29 -0800

    We have a working version. Need to work on CSS functionality

--- public/script.js
+++ public/script.js
@@ -1,4 +1,6 @@
 console.log("Script Started");
+
+
 var $exes = $('.glyphicon-remove')
 var $stars = $('.glyphicon-star')
 var $checkboxes = $('.checkboxes')
@@ -7,8 +9,23 @@ var $paras = $('p')
 var text = $('#todo')
 
 function removeDiv() {
+
+
   $(this).parent().hide('slow');
+  //$(this).parent().remove();
+
+  $.ajax({
+    url: 'api/todos/' + $(this).parent().attr('id') ,
+    method: 'DELETE',
+    success: function(data){
+        console.log('it is deleted');
+    },
+    error: function(err) {
+      console.log(err);
+    } 
+   });
 }
+
 $exes.click(removeDiv);
 
 function colorStar(){
@@ -22,7 +39,33 @@ function strikethrough(){
 
 $checkboxes.click(strikethrough);
 
+/*When window loads, we need to fill the list with todos from database*/
+window.onload = function() {
 
+  $.ajax({
+    url: 'api/todos',
+    method: 'GET',
+     success: function(data){
+      for (var i=0; i<data.length; i++) {
+
+          $('.list').last().append('<p id='+ data[i]._id +'><input type="checkbox" class="checkboxes"><i class="glyphicon glyphicon-star"></i><span>'+ data[i].entry +'</span><i class="glyphicon glyphicon-remove"></i></p>');
+          $('p').last().hide();
+          $('p').last().show('slow');
+          $exes = $('.glyphicon-remove')
+          $star = $('.glyphicon-star')
+          $checkboxes = $('.checkboxes')
+          $span = $('span')
+          $paras = $('p')
+          $exes.click(removeDiv);
+          $stars.click(colorStar);
+          $checkboxes.click(strikethrough);
+
+      }/*end of for loop*/
+     }/*end of success function*/
+   });
+
+
+}
 
 
 /*THIS IS THE ADD BUTTON WE USE AJAX HERE*/
@@ -52,6 +95,7 @@ $('.btn').click(function(){
    /**********************/
 
   text.val('');
+
   $('p').last().hide();
   $('p').last().show('slow');
   $exes = $('.glyphicon-remove')
